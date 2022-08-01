c14fa559bf16760d88ec38791a41286864d818e8
koki <zghong@huanbaoyun.cloud>
2022-08-01 18:36:12 +0800

    fix: error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import yayJpg from "../assets/yay.jpg";
 export default function HomePage() {
   return (
     <div>
-      {NAME}
       <h2>Yay! Welcome to umi!</h2>
       <p>
         <img src={yayJpg} width="388" />
